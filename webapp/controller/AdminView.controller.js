dce78f3104f44dbb9531b360e2b4db515540c0e4
Noa Junod <noa.junod@sap.com>
2019-12-06 13:55:28 +0000

    renaming

--- webapp/controller/AdminView.controller.js
+++ webapp/controller/AdminView.controller.js
@@ -1,45 +1,47 @@
 sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
 	"use strict";
-	return Controller.extend("MarketingToolTestSpace.MarketingToolTest.controller.adminView", {
+	return Controller.extend("TacticTool.controller.AdminView", {
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf MarketingToolTestSpace.MarketingToolTest.view.adminView
+		 * @memberOf TacticTool.view.AdminView
 		 */
 		onInit: function () {
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			oRouter.getRoute("adminView").attachMatched(this._onRouteMatched, this);
+			oRouter.getRoute("AdminView").attachMatched(this._onRouteMatched, this);
 		},
+
 		navBack: function () {
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			oRouter.navTo("TargetdetailedView", true);
+			oRouter.navTo("TargetView1", true);
 		}
-			/**
-			 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-			 * (NOT before the first rendering! onInit() is used for that one!).
-			 * @memberOf MarketingToolTestSpace.MarketingToolTest.view.adminView
-			 */
-			//	onBeforeRendering: function() {
-			//
-			//	},
-			/**
-			 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-			 * This hook is the same one that SAPUI5 controls get after being rendered.
-			 * @memberOf MarketingToolTestSpace.MarketingToolTest.view.adminView
-			 */
-			//	onAfterRendering: function() {
-			//
-			//	},
-			/**
-			 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-			 * @memberOf MarketingToolTestSpace.MarketingToolTest.view.adminView
-			 */
-			//	onExit: function() {
-			//
-			//	}
-			
+
 		/**
-		 *@memberOf MarketingToolTestSpace.MarketingToolTest.controller.adminView
+		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+		 * (NOT before the first rendering! onInit() is used for that one!).
+		 * @memberOf TacticTool.view.AdminView
+		 */
+		//	onBeforeRendering: function() {
+		//
+		//	},
+		/**
+		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+		 * This hook is the same one that SAPUI5 controls get after being rendered.
+		 * @memberOf TacticTool.view.AdminView
+		 */
+		//	onAfterRendering: function() {
+		//
+		//	},
+		/**
+		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+		 * @memberOf TacticTool.view.AdminView
+		 */
+		//	onExit: function() {
+		//
+		//	}
+
+		/**
+		 *@memberOf TacticTool.controller.AdminView
 		 */
 
 	});
