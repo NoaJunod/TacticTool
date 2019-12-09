cda57a1ad43cfc238e3dbcee70256c29077f25b1
Noa Junod <noa.junod@sap.com>
2019-12-09 13:52:41 +0000

    Navigation

--- webapp/controller/AdminView.controller.js
+++ webapp/controller/AdminView.controller.js
@@ -15,64 +15,6 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.navTo("TargetView1", true);
 		}
-<<<<<<< HEAD
-			/**
-			 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-			 * (NOT before the first rendering! onInit() is used for that one!).
-			 * @memberOf TacticTool.view.AdminView
-			 */
-			//	onBeforeRendering: function() {
-			//
-			//	},
-			/**
-			 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-			 * This hook is the same one that SAPUI5 controls get after being rendered.
-			 * @memberOf TacticTool.view.AdminView
-			 */
-			//	onAfterRendering: function() {
-			//
-			//	},
-			/**
-			 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-			 * @memberOf TacticTool.view.AdminView
-			 */
-			//	onExit: function() {
-			//
-			//	}
-			
-=======
-
->>>>>>> refs/heads/master
-		/**
-<<<<<<< HEAD
-=======
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf TacticTool.view.AdminView
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf TacticTool.view.AdminView
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf TacticTool.view.AdminView
-		 */
-		//	onExit: function() {
-		//
-		//	}
-
-		/**
->>>>>>> refs/heads/master
-		 *@memberOf TacticTool.controller.AdminView
-		 */
 
 	});
 });
